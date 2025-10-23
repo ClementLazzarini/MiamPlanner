0b545b205ff1eb7d2f58cf7355b8b6e77352b468
Clement Lazzarini <clement.lazzarini71@gmail.com>
2025-10-23 14:36:54 +0200

    Add Tailwind CSS integration and theme app
    Integrated Tailwind CSS by adding django-tailwind and related dependencies, created a new 'theme' app with configuration and static files, and updated settings.py. Refactored templates to use a base layout and Tailwind classes for improved styling.

---
+++ theme/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        // --- Ajoute les chemins vers TOUS tes templates ---
+        '../../recettes/templates/**/*.html',
+        '../../templates/**/*.html', // Pour ton futur base.html
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
